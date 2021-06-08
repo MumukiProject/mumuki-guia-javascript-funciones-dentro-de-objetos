2f926a13a3afb6262160d428d6a4a11d3cfa56cd
mayra@mumuki.org <mayra@mumuki.org>
2021-06-08 18:15:11 +0000

    Mumuki Export on 2021-06-08 18:15:11 +0000

--- 00002_Objetos II/test..js
+++ 00002_Objetos II/test..js
@@ -1,23 +1,15 @@
 describe("", function() {
   it("Si tenemos una pelicula de nombre The Dark Knight y año 2008 al hacer pelicula.mostrarInfo() retorna Película: The Dark Knight - Año: 2008", function() {
-    let pelicula = {
-    titulo: "The Dark Knight",
-    anio: 2008,
-    director: "Christopher Nolan",
-};
-    let resultado = pelicula.mostrarInfo();
-    assert.equal(resultado,'Película: The Dark Knight - Año: 2008');
+    pelicula.titulo: "The Dark Knight",
+    pelicula.anio: 2008,
+    assert.equal(pelicula.mostrarInfo(),'Película: The Dark Knight - Año: 2008');
   })
 });
 
 describe("", function() {
   it("Si tenemos una pelicula de nombre El laberinto del fauno y año 2006 al hacer pelicula.mostrarInfo() retorna Película: El laberinto del fauno - Año: 2006", function() {
-    let pelicula = {
-    titulo: "El laberinto del fauno",
-    anio: 2006,
-    director: "Guillermo del Toro",
-};
-    let resultado = pelicula.mostrarInfo();
-    assert.equal(resultado,'Película: El laberinto del fauno - Año: 2006');
+    pelicula.titulo: "El laberinto del fauno",
+    pelicula.anio: 2006,
+    assert.equal(pelicula.mostrarInfo(),'Película: El laberinto del fauno - Año: 2006');
   })
 });
