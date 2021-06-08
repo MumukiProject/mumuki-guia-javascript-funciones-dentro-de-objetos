d103d0d05eba466cd11285eb6bacfa4b34f6cea4
mayra@mumuki.org <mayra@mumuki.org>
2021-06-08 18:20:43 +0000

    Mumuki Export on 2021-06-08 18:20:43 +0000

--- 00004_Objetos IV/test..js
+++ 00004_Objetos IV/test..js
@@ -2,8 +2,7 @@ describe("", function() {
   it("Si tenemos un perfil que tiene nombre Hedy y apellido Lamarr, al hacer perfil.obtenerNombreCompleto() retorna Hedy Lamarr", function() {
       perfil.nombre = "Hedy",
       perfil.apellido = "Lamarr",
-    };
-    assert.equal(perfil.obtenerNombreCompleto,'Hedy Lamarr');
+      assert.equal(perfil.obtenerNombreCompleto,'Hedy Lamarr');
   })
 });
 
@@ -11,7 +10,6 @@ describe("", function() {
   it("Si tenemos un perfil que tiene nombre Marilina y apellido Bertoldi, al hacer perfil.obtenerNombreCompleto() retorna Marilina Bertoldi", function() {
       perfil.nombre = "Marilina",
       perfil.apellido = "Bertoldi",
-    };
-    assert.equal(perfil.obtenerNombreCompleto,'Marilina Bertoldi');
+      assert.equal(perfil.obtenerNombreCompleto,'Marilina Bertoldi');
   })
 });
