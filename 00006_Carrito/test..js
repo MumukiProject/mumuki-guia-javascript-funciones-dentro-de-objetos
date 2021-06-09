7597057a4d04ce6011bf742ed633092e81bc50e2
mayra@mumuki.org <mayra@mumuki.org>
2021-06-09 19:13:20 +0000

    Mumuki Export on 2021-06-09 19:13:20 +0000

--- 00006_Carrito/test..js
+++ 00006_Carrito/test..js
@@ -1,5 +1,17 @@
 describe("", function() {
   it("Si agregamos un mouse, y un teclado el carrito tiene estos dos productos", function() {
+    let mouse = {
+      id: 0,
+      titulo: "Mouse Genius",
+      precio: 150,
+      envioIncluido: false,
+    };
+      let teclado = {
+      id: 0,
+      titulo: "Mouse Genius",
+      precio: 300,
+      envioIncluido: false,
+    };
       carrito.agregarAlCarrito(mouse);
       carrito.agregarAlCarrito(teclado);
       assert.deepEqual(carrito.productos(),[mouse, teclado]);
@@ -8,6 +20,18 @@ describe("", function() {
 
 describe("", function() {
   it("Si agregamos un mouse de 150, y un teclado de 300, al hacer carrito.obtenerTotal retorna 450", function() {
+    let mouse = {
+      id: 0,
+      titulo: "Mouse Genius",
+      precio: 150,
+      envioIncluido: false,
+    };
+      let teclado = {
+      id: 0,
+      titulo: "Mouse Genius",
+      precio: 300,
+      envioIncluido: false,
+    };
       carrito.agregarAlCarrito(mouse);
       carrito.agregarAlCarrito(teclado);
       assert.equal(carrito.obtenerTotal(), 450);
