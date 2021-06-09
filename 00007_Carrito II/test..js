b957cdc61e2b2826130a48dfac7d9c00315566c7
mayra@mumuki.org <mayra@mumuki.org>
2021-06-09 19:55:27 +0000

    Mumuki Export on 2021-06-09 19:55:27 +0000

--- 00007_Carrito II/test..js
+++ 00007_Carrito II/test..js
@@ -1,1 +1,31 @@
-TODO
+describe("", function() {
+  it("Si agregamos un mouse con envio incluido el precio se mantiene igual", function() {
+    let mouse = {
+      id: 0,
+      titulo: "Mouse Genius",
+      precio: 150,
+      envioIncluido: false,
+    };
+      carrito.agregarAlCarrito(mouse);
+      assert.equal(mouse.precio,150);
+  })
+});
+
+describe("", function() {
+  it("Si agregamos un teclado que no tiene envio incluido, se le suman 120 al precio del producto", function() {
+    let teclado = {
+      id: 0,
+      titulo: "teclado Genius",
+      precio: 300,
+      envioIncluido: false,
+    };
+      carrito.agregarAlCarrito(teclado);
+      assert.equal(teclado.precio, 420);
+  })
+});
+
+describe("", function() {
+  it("Si agregamos un mouse de 150 y un teclado de 420 más envio al hacer precioConIVA retorna 689.7", function() {
+      assert.equal(carrito.precioConIVA, 689.7);
+  })
+});
