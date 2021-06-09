0a8ed18b58f13441162754d6a4c6b926ce55184d
mayra@mumuki.org <mayra@mumuki.org>
2021-06-09 19:20:44 +0000

    Mumuki Export on 2021-06-09 19:20:44 +0000

--- 00006_Carrito/test..js
+++ 00006_Carrito/test..js
@@ -14,7 +14,7 @@ describe("", function() {
     };
       carrito.agregarAlCarrito(mouse);
       carrito.agregarAlCarrito(teclado);
-      assert.deepEqual(carrito.productos(),[mouse, teclado]);
+      assert.deepEqual(carrito.productos,[mouse, teclado]);
   })
 });
 
@@ -43,6 +43,6 @@ describe("", function() {
       carrito.productos = [];
       carrito.agregarAlCarrito(monitor);
       carrito.agregarAlCarrito(auriculares);
-      assert.deepEqual(carrito.productos(),[monitor, auriculares]);
+      assert.deepEqual(carrito.productos, [monitor, auriculares]);
   })
 });
