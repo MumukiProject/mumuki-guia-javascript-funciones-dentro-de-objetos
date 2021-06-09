78bc66efcd384a35a4f3b370c632a90d39d298f7
mayra@mumuki.org <mayra@mumuki.org>
2021-06-09 19:28:12 +0000

    Mumuki Export on 2021-06-09 19:28:12 +0000

--- 00006_Carrito/test..js
+++ 00006_Carrito/test..js
@@ -20,20 +20,6 @@ describe("", function() {
 
 describe("", function() {
   it("Si agregamos un mouse de 150, y un teclado de 300, al hacer carrito.obtenerTotal retorna 450", function() {
-    let mouse = {
-      id: 0,
-      titulo: "Mouse Genius",
-      precio: 150,
-      envioIncluido: false,
-    };
-      let teclado = {
-      id: 0,
-      titulo: "Mouse Genius",
-      precio: 300,
-      envioIncluido: false,
-    };
-      carrito.agregarAlCarrito(mouse);
-      carrito.agregarAlCarrito(teclado);
       assert.equal(carrito.obtenerTotal(), 450);
   })
 });
