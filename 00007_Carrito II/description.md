9e0fd540d5410c48e33de93557bb190808e9cb9e
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-14 19:43:25 +0000

    Mumuki Export on 2021-06-14 19:43:25 +0000

--- 00007_Carrito II/description.md
+++ 00007_Carrito II/description.md
@@ -1,6 +1,6 @@
 Debido a un cambio de requerimiento, el precio final total se calcula distinto.
 Si un producto no tiene `envioIncluido` entonces al precio del producto hay que sumarle 120.
 Si tiene `envioIncluido` entonces se toma el precio que está en el objeto y no se le suma nada.
-Una vez calculado el total, hay que agregar un 21% de IVA.
+Una vez calculado el total, hay que agregar un 21% de IVA. :moneybag:
 
 > Modificá la función `obtenerTotal` para que retorne el precio total más el IVA.
--- 00007_Carrito II/test..js
+++ 00007_Carrito II/test..js
@@ -1,31 +1,38 @@
 describe("", function() {
+  let mouse = {
+    id: 1,
+    titulo: "Mouse Genius",
+    precio: 150,
+    envioIncluido: true,
+  };
+  
+  let teclado = {
+    id: 2,
+    titulo: "teclado Genius",
+    precio: 300,
+    envioIncluido: false,
+  };
+  
   it("Si agregamos un mouse con envio incluido el precio se mantiene igual", function() {
-    let mouse = {
-      id: 0,
-      titulo: "Mouse Genius",
-      precio: 150,
-      envioIncluido: true,
-    };
-      carrito.agregarAlCarrito(mouse);
-      assert.equal(mouse.precio,150);
+    carrito.productos = [];
+    carrito.agregarAlCarrito(mouse);
+    assert.equal(mouse.precio,150);
   })
 });
 
 describe("", function() {
   it("Si agregamos un teclado que no tiene envio incluido, se le suman 120 al precio del producto", function() {
-    let teclado = {
-      id: 0,
-      titulo: "teclado Genius",
-      precio: 300,
-      envioIncluido: false,
-    };
-      carrito.agregarAlCarrito(teclado);
-      assert.equal(teclado.precio, 420);
+    carrito.productos = [];
+    carrito.agregarAlCarrito(teclado);
+    assert.equal(teclado.precio, 420);
   })
 });
 
 describe("", function() {
   it("Si agregamos un mouse de 150 y un teclado de 420 más envio al hacer precioConIVA retorna 689.7", function() {
-      assert.equal(carrito.obtenerTotal(), 689.7);
+    carrito.productos = [];
+    carrito.agregarAlCarrito(mouse);
+    carrito.agregarAlCarrito(teclado);
+    assert.equal(carrito.obtenerTotal(), 689.7);
   })
 });
