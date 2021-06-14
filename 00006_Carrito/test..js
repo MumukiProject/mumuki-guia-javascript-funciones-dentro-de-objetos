fb3d4e2247d8c910d11d1e2bf7a84b0792834145
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-14 19:24:40 +0000

    Mumuki Export on 2021-06-14 19:24:40 +0000

--- 00006_Carrito/test..js
+++ 00006_Carrito/test..js
@@ -1,4 +1,4 @@
-describe("Si agregamos un mouse de 150, y un teclado de 300", function() {
+describe("Si agregamos un mouse de 150 y un teclado de 300", function() {
   let mouse = {
     id: 1,
     titulo: "Mouse Genius",
@@ -18,33 +18,30 @@ describe("Si agregamos un mouse de 150, y un teclado de 300", function() {
   })
 
   it("al hacer carrito.obtenerTotal retorna 450", function() {
-      assert.equal(carrito.obtenerTotal(), 450);
+    assert.equal(carrito.obtenerTotal(), 450);
   })
 });
 
-describe("", function() {
-  it("Si agregamos un monitor, y unos auriculares el carrito tiene estos dos productos", function() {
-      let monitor = {
-      id: 3,
-      titulo: "Monitor Genius",
-      precio: 2500,
-      envioIncluido: false,
-    };
-      let auriculares = {
-      id: 4,
-      titulo: "Auriculares Genius",
-      precio: 500,
-      envioIncluido: false,
-    };
-      carrito.productos = [];
-      carrito.agregarAlCarrito(monitor);
-      carrito.agregarAlCarrito(auriculares);
-      assert.deepEqual(carrito.productos, [monitor, auriculares]);
+describe("Si agregamos un monitor de 2500 y unos auriculares de 500", function() {
+  let monitor = {
+    id: 3,
+    titulo: "Monitor Genius",
+    precio: 2500,
+    envioIncluido: false,
+  };
+  let auriculares = {
+    id: 4,
+    titulo: "Auriculares Genius",
+    precio: 500,
+    envioIncluido: false,
+  };
+  carrito.agregarAlCarrito(monitor);
+  carrito.agregarAlCarrito(auriculares);
+  it("el carrito tiene estos dos productos", function() {
+    assert.deepEqual(carrito.productos, [monitor, auriculares]);
   })
-});
 
-describe("", function() {
-  it("Si agregamos un monitor de 2500, y unos auriculares de 500, al hacer carrito.obtenerTotal retorna 3000", function() {
-      assert.equal(carrito.obtenerTotal(), 3000);
+  it("al hacer carrito.obtenerTotal retorna 3000", function() {
+    assert.equal(carrito.obtenerTotal(), 3000);
   })
 });
