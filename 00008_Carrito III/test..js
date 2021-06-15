1d56dea1e686c9c5d9d695295bb72cb0f9d022e4
mayra@mumuki.org <mayra@mumuki.org>
2021-06-15 15:41:50 +0000

    Mumuki Export on 2021-06-15 15:41:50 +0000

--- 00008_Carrito III/test..js
+++ 00008_Carrito III/test..js
@@ -1,2 +1,33 @@
-TODO
-
+describe("Si agregamos un mouse, un teclado y unos auriculares", function() {
+  let mouse = {
+    id: 1,
+    titulo: "Mouse Genius",
+    precio: 150,
+    envioIncluido: false,
+  };
+  let teclado = {
+    id: 2,
+    titulo: "Teclado Genius",
+    precio: 300,
+    envioIncluido: false,
+  };
+  let monitor = {
+    id: 3,
+    titulo: "Monitor Genius",
+    precio: 2500,
+    envioIncluido: false,
+  };
+  let auriculares = {
+    id: 4,
+    titulo: "Auriculares Genius",
+    precio: 500,
+    envioIncluido: false,
+  };
+  it("y hacemos carrito.detalle() nos retorna el listado con el formato correcto de los tres productos", function() {
+    carrito.productos = [];
+    carrito.agregarAlCarrito(mouse);
+    carrito.agregarAlCarrito(teclado);
+    carrito.agregarAlCarrito(auriculares);
+    assert.equal(carrito.detalle(), 'Listado de productos:\nMouse Genius - 270\n,Auriculares Genius - 620\n,Teclado Genius - 420\nTotal con IVA: 1585.1');
+  })
+  
