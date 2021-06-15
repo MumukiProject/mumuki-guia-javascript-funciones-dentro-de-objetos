2231c54185466237b84174f1884fe6058838ee71
gustavo@mumuki.org <gustavo@mumuki.org>
2021-06-15 19:16:30 +0000

    Mumuki Export on 2021-06-15 19:16:30 +0000

--- 00008_Carrito III/test..js
+++ 00008_Carrito III/test..js
@@ -9,7 +9,7 @@ describe("Si agregamos un monitor", function() {
   it("y hacemos carrito.detalle() nos retorna el monitor con el detalle de la compra", function() {
     carrito.productos = [];
     carrito.agregarAlCarrito(monitor);
-    assert.equal(carrito.detalle(), 'Listado de productos:\nMonitor Genius - 2620\nTotal con IVA: 3170.2');
+    assert.equal(carrito.detalle(), 'Listado de productos:\nMonitor Genius - $2620\nTotal con IVA: $3170.2');
   })
   
 });
@@ -37,7 +37,7 @@ describe("Si agregamos un mouse, unos auriculares y un teclado", function() {
     carrito.agregarAlCarrito(mouse);
     carrito.agregarAlCarrito(auriculares);
     carrito.agregarAlCarrito(teclado);
-    assert.equal(carrito.detalle(), 'Listado de productos:\nMouse Genius - 270\n,Auriculares Genius - 620\n,Teclado Genius - 420\nTotal con IVA: 1585.1');
+    assert.equal(carrito.detalle(), 'Listado de productos:\nMouse Genius - $270\n,Auriculares Genius - 620\n,Teclado Genius - $420\nTotal con IVA: $1585.1');
   })
   
 });
