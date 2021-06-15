28fc98607b0b25c58602a0fd105e879d077add5b
mayra@mumuki.org <mayra@mumuki.org>
2021-06-15 15:53:07 +0000

    Mumuki Export on 2021-06-15 15:53:07 +0000

--- 00008_Carrito III/test..js
+++ 00008_Carrito III/test..js
@@ -1,4 +1,19 @@
-describe("Si agregamos un mouse,", function() {
+describe("Si agregamos un monitor", function() {
+
+  let monitor = {
+    id: 3,
+    titulo: "Monitor Genius",
+    precio: 2500,
+    envioIncluido: false,
+  };
+  it("y hacemos carrito.detalle() nos retorna el monitor con el detalle de la compra", function() {
+    carrito.productos = [];
+    carrito.agregarAlCarrito(monitor);
+    assert.equal(carrito.detalle(), 'Listado de productos:\nMonitor Genius - 2620\nTotal con IVA: 3170.2');
+  })
+  
+});
+describe("Si agregamos un mouse, unos auriculares y un teclado", function() {
   let mouse = {
     id: 1,
     titulo: "Mouse Genius",
@@ -9,12 +24,6 @@ describe("Si agregamos un mouse,", function() {
     id: 2,
     titulo: "Teclado Genius",
     precio: 300,
-    envioIncluido: false,
-  };
-  let monitor = {
-    id: 3,
-    titulo: "Monitor Genius",
-    precio: 2500,
     envioIncluido: false,
   };
   let auriculares = {
